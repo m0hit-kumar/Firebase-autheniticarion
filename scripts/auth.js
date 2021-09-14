ee968914ec09e1059493694b8019c2b98c4af159
m0hit kumar <mohitkumar11725@gmail.com>
2021-09-15 00:01:41 +0530

    favicion

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firefirebasejs/9.0.2/firebase-firestore.js";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyApDLiOvCkuhozs-7_ebxCkeYRbaaPaClw",
+  authDomain: "opp-online-placement-portal.firebaseapp.com",
+  projectId: "opp-online-placement-portal",
+  storageBucket: "opp-online-placement-portal.appspot.com",
+  messagingSenderId: "1018033370321",
+  appId: "1:1018033370321:web:063832ae90f0b9bb6c01f0",
+});
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+// db.settings({ timestampsInSnapshots: true });
+
 const signupform = document.querySelector("#signup-form");
 // console.log(signupform);
 signupform.addEventListener("submit", (e) => {
@@ -12,7 +35,12 @@ signupform.addEventListener("submit", (e) => {
 
   // signup the users
 
-  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred);
-  });
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log(userCredential);
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(erroe.message);
+    });
 });
